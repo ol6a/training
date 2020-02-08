1c1e133a5c0bb812be403673608a18b0e349e158
ol6a <fedorova.o.a@bk.ru>
2020-02-08 18:05:44 +0300

    выделены вспомогательные методы и класс

---
+++ fiz_contact.py
@@ -0,0 +1,9 @@
+class Fiz_contact:
+    def __init__(self, lastname, firstname, middlename, email, telephone, password, confirmpassword):
+        self.lastname=lastname
+        self.firstname=firstname
+        self.middlename=middlename
+        self.email=email
+        self.telephone=telephone
+        self.password=password
+        self.confirmpassword=confirmpassword
