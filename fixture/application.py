1eaaf751ff7bc0cf46ad1e32330d988c1a700da1
ol6a <fedorova.o.a@bk.ru>
2020-02-09 00:11:59 +0300

    Внесены изменения для работы с классами помощниками

--- fixture/application.py
+++ fixture/application.py
@@ -1,33 +1,17 @@
-
+from fixture.session import SessionHelper
+from fixture.fizzakaz import FizZakazHelper
 from selenium import webdriver
 
 class Application:
     def __init__(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
+        self.session = SessionHelper(self)
+        self.fizzakaz = FizZakazHelper(self)
 
     def go_to_home(self):
         wd = self.wd
         wd.get("https://test01.moi-uni.ru/")
-
-    def confirm_order(self):
-        wd = self.wd
-        wd.find_element_by_id("button-confirm").click()
-
-    def oplata_kvit(self):
-        wd = self.wd
-        wd.find_element_by_link_text("скачать квитанцию").click()
-        wd.find_element_by_id("button-payment-method").click()
-
-
-
-    def payment_address(self):
-        wd = self.wd
-        wd.find_element_by_id("button-payment-address").click()
-
-    def registration_order(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Оформить заказ").click()
 
     def select_course(self):
         wd = self.wd
@@ -41,41 +25,12 @@ class Application:
         wd = self.wd
         wd.find_element_by_xpath("//ul[@id='osnovnoe-menu']/li[2]/a/span").click()
 
-    def registration_fiz(self, fiz_contact):
-        wd = self.wd
-        wd.find_element_by_link_text("Регистрация").click()
-        wd.find_element_by_id("input-lastname").click()
-        wd.find_element_by_xpath("//fieldset[@id='account']/div[2]").click()
-        wd.find_element_by_id("input-lastname").clear()
-        wd.find_element_by_id("input-lastname").send_keys(fiz_contact.lastname)
-        wd.find_element_by_id("input-firstname").click()
-        wd.find_element_by_id("input-firstname").clear()
-        wd.find_element_by_id("input-firstname").send_keys(fiz_contact.firstname)
-        wd.find_element_by_id("input-middlename").click()
-        wd.find_element_by_id("input-middlename").clear()
-        wd.find_element_by_id("input-middlename").send_keys(fiz_contact.middlename)
-        wd.find_element_by_id("input-email").click()
-        wd.find_element_by_id("input-email").clear()
-        wd.find_element_by_id("input-email").send_keys(fiz_contact.email)
-        wd.find_element_by_id("input-telephone").click()
-        wd.find_element_by_id("input-telephone").clear()
-        wd.find_element_by_id("input-telephone").send_keys(fiz_contact.telephone)
-        wd.find_element_by_id("input-password").click()
-        wd.find_element_by_id("input-password").clear()
-        wd.find_element_by_id("input-password").send_keys(fiz_contact.password)
-        wd.find_element_by_id("input-confirm").click()
-        wd.find_element_by_id("input-confirm").clear()
-        wd.find_element_by_id("input-confirm").send_keys(fiz_contact.confirmpassword)
-        wd.find_element_by_name("handle_agree").click()
-        wd.find_element_by_xpath(u"//input[@value='Продолжить']").click()
-
     def is_element_present(self, how, what):
         try:
             self.wd.find_element(by=how, value=what)
         except NoSuchElementException as e:
             return False
         return True
-
 
 
     def close_alert_and_get_its_text(self):
@@ -90,10 +45,6 @@ class Application:
         finally:
             self.accept_next_alert = True
 
-
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_xpath("(//a[contains(text(),'Выход')])[3]").click()
 
     def destroy(self):
         self.wd.quit()
--- test/reg_zakaz_fiz.py
+++ test/reg_zakaz_fiz.py
@@ -4,7 +4,6 @@ import pytest
 from fixture.application import Application
 from model.fiz_contact import Fiz_contact
 
-
 @pytest.fixture
 def app(request):
     fixture = Application()
@@ -13,16 +12,16 @@ def app(request):
 
 def test_regfiz_zakaz(app):
     app.go_to_home()
-    app.registration_fiz(Fiz_contact("мтест167", "мтест167", "мтест167", "m167@moi-uni.ru", "89213456789", "12345", "12345"))
+    app.session.registration_fiz(Fiz_contact("мтест169", "мтест169", "мтест169", "m169@moi-uni.ru", "89213456789", "12345", "12345"))
     app.go_to_catalog()
     app.go_to_number_category()
     app.select_course()
-    app.registration_order()
-    app.payment_address()
-    app.payment_address()
-    app.oplata_kvit()
-    app.confirm_order()
-    app.logout()
+    app.fizzakaz.registration_order()
+    app.fizzakaz.payment_address()
+    app.fizzakaz.payment_address()
+    app.fizzakaz.oplata_kvit()
+    app.fizzakaz.confirm_order()
+    app.session.logout()
 
 
 
